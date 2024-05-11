7466ffe1bdc46da18cfe0aa10b554d5075f9205d
Zia Uddin Ahmed <zia.ete10@gmail.com>
2024-05-11 11:14:17 +0600

    UPDATE: Adds Application Db Context

---
+++ Ecom.Services.CouponApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Ecom.Services.CouponApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecom.Services.CouponApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
--- Ecom.Services.CouponApi/Models/Coupon.cs
+++ Ecom.Services.CouponApi/Models/Coupon.cs
@@ -1,9 +1,14 @@
-﻿namespace Ecom.Services.CouponApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecom.Services.CouponApi.Models
 {
     public class Coupon
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Code { get; set; }
+        [Required]
         public double DiscountAmount { get; set; }
         public int MinAmount { get; set; }
         public DateTime LastUpdated { get; set; }
