2ec9e267b4346f6f1b8cd80b3ad674479e291b0d
Zia Uddin Ahmed <zia.ete10@gmail.com>
2024-05-17 23:53:44 +0600

    UPDATE: Adds init cart api controller

---
+++ Ecom.Services.ShoppingCartApi/Controllers/CartAPIController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using AutoMapper;
+using Ecom.Services.ShoppingCartApi.Data;
+using Ecom.Services.ShoppingCartApi.Models.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CartAPIController :ControllerBase
+{
+    private readonly ApplicationDbContext _applicationDbContext;
+    public ResponseDto _responseDto;
+    private IMapper _mapper;
+    public CartAPIController(ApplicationDbContext applicationDbContext, IMapper mapper)
+    {
+        _applicationDbContext = applicationDbContext;
+        _responseDto = new ResponseDto();
+        _mapper = mapper;
+    }
+
+    [HttpPost("CartUpsert")]
+    public async Task<ResponseDto> CartUpsert(CartDto cartDto)
+    {
+        try
+        {
+            //Cart cart = _mapper.Map<Cart>(cartDto);
+            //_responseDto.Result = _mapper.Map<CartDto>(cart);
+        }
+        catch (Exception ex)
+        {
+            _responseDto.IsSuccess = false;
+            _responseDto.Message = ex.Message;
+        }
+        return _responseDto;
+    }
+}
