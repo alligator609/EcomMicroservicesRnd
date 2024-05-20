e00932e13921a99fae1a43be674bcf2be50d7b20
Zia Uddin Ahmed <zia.ete10@gmail.com>
2024-05-20 22:03:08 +0600

    UPDATE: Adds Message Bus class Library

---
+++ Ecpm.MessageBus/MessageBus.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+namespace Ecpm.MessageBus
+{
+    public class MessageBus : IMessageBus
+    {
+        private readonly string _connectionString;
+        public async Task PublishMessage(object message, string topic_queue_name = null)
+        {
+            string connectionString = "Endpoint=sb://ecpm-servicebus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Z6Q5Q5Q5Q5";   
+            await using var client = new ServiceBusClient(connectionString);
+
+            var sender = client.CreateSender(topic_queue_name);
+            var jsonMessage = System.Text.Json.JsonSerializer.Serialize(message);
+
+            var messageBusMessage = new ServiceBusMessage(Encoding.UTF8.GetBytes(jsonMessage)){
+                CorrelationId = Guid.NewGuid().ToString()
+            };
+            await sender.SendMessageAsync(messageBusMessage);
+            await client.DisposeAsync();
+        }
+    }
+}
